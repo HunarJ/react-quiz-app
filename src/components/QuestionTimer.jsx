c01b831c52e45dfcdd1b1e97f371b9f4d624161c
Jan Hunar <jan.hunar.jha@gmail.com>
2024-07-25 14:26:04 +0200

    RGP-2: Timers

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+export const QuestionTimer = ({ timeout, onTimeout }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect((first) => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+        clearTimeout(timer)
+    }
+
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+        clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+};
